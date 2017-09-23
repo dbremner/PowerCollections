2b268e16891cbcd227c8738d9ed16335c8de59be
David Bremner <dbremner@gmail.com>
2017-09-23 00:44:00 -0700

    copy a method from Imms

---
+++ Source/UnitTestsVSTS/Kvp.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Wintellect.PowerCollections.Tests {
+    internal static class Kvp {
+        public static KeyValuePair<TKey, TValue> Of<TKey, TValue>(TKey key, TValue value) {
+            return new KeyValuePair<TKey, TValue>(key, value);
+        }
+    }
+}
