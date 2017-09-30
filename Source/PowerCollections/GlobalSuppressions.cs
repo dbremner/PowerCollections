4cd5cb55b6ac53758710e23dab343855119e59db
David Bremner <dbremner@gmail.com>
2017-09-29 22:36:42 -0700

    suppress warnings

---
+++ Source/PowerCollections/GlobalSuppressions.cs
@@ -0,0 +1,24 @@
+﻿[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Scope = "member", Target = "Wintellect.PowerCollections.Algorithms.#GetBagEqualityComparer`1()")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Scope = "member", Target = "Wintellect.PowerCollections.Algorithms.#GetBagEqualityComparer`1(System.Collections.Generic.IEqualityComparer`1<!!0>)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Scope = "member", Target = "Wintellect.PowerCollections.Algorithms.#GetCollectionEqualityComparer`1()")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Scope = "member", Target = "Wintellect.PowerCollections.Algorithms.#GetCollectionEqualityComparer`1(System.Collections.Generic.IEqualityComparer`1<!!0>)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Scope = "member", Target = "Wintellect.PowerCollections.Algorithms.#GetLexicographicalComparer`1(System.Comparison`1<!!0>)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Scope = "member", Target = "Wintellect.PowerCollections.Algorithms.#GetLexicographicalComparer`1(System.Collections.Generic.IComparer`1<!!0>)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Scope = "member", Target = "Wintellect.PowerCollections.MultiDictionaryBase`2.#KeyValuePairs")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Scope = "member", Target = "Wintellect.PowerCollections.OrderedDictionary`2.#AddMany(System.Collections.Generic.IEnumerable`1<System.Collections.Generic.KeyValuePair`2<!0,!1>>)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Scope = "member", Target = "Wintellect.PowerCollections.OrderedDictionary`2.#.ctor(System.Collections.Generic.IEnumerable`1<System.Collections.Generic.KeyValuePair`2<!0,!1>>)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Scope = "member", Target = "Wintellect.PowerCollections.OrderedDictionary`2.#.ctor(System.Collections.Generic.IEnumerable`1<System.Collections.Generic.KeyValuePair`2<!0,!1>>,System.Comparison`1<!0>)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Scope = "member", Target = "Wintellect.PowerCollections.OrderedDictionary`2.#.ctor(System.Collections.Generic.IEnumerable`1<System.Collections.Generic.KeyValuePair`2<!0,!1>>,System.Collections.Generic.IComparer`1<!0>)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Scope = "member", Target = "Wintellect.PowerCollections.ReadOnlyMultiDictionaryBase`2.#KeyValuePairs")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Scope = "member", Target = "Wintellect.PowerCollections.Algorithms.#CartesianProduct`2(System.Collections.Generic.IEnumerable`1<!!0>,System.Collections.Generic.IEnumerable`1<!!1>)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Scope = "member", Target = "Wintellect.PowerCollections.Algorithms.#GetLexicographicalComparer`1()")]
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+//
+// To add a suppression to this file, right-click the message in the 
+// Code Analysis results, point to "Suppress Message", and click 
+// "In Suppression File".
+// You do not need to add suppressions to this file manually.
+
